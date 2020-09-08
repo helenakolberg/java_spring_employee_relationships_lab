adedc773133fe4bec084a5a3bfcea2e502a77f1a
Helena <inger.helena@gmail.com>
2020-09-08 11:13:36 +0100

    created Employee class, getters & setters, table &  columns

--- src/main/java/com/codeclan/example/employeeService/models/Employee.java
+++ src/main/java/com/codeclan/example/employeeService/models/Employee.java
@@ -1,4 +1,75 @@
 package com.codeclan.example.employeeService.models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "number")
+    private String number;
+
+    private List<Project> projects;
+
+    public Employee(String firstName, String lastName, String number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.number = number;
+        this.projects = new ArrayList<Project>();
+    }
+
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
 }

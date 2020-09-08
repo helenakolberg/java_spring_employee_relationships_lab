aed7e2076a595c58ce0f8792a3011b2972238356
Helena <inger.helena@gmail.com>
2020-09-08 11:45:47 +0100

    two tests pass

--- src/test/java/com/codeclan/example/employeeService/EmployeeServiceApplicationTests.java
+++ src/test/java/com/codeclan/example/employeeService/EmployeeServiceApplicationTests.java
@@ -1,13 +1,40 @@
 package com.codeclan.example.employeeService;
 
+import com.codeclan.example.employeeService.models.Department;
+import com.codeclan.example.employeeService.models.Employee;
+import com.codeclan.example.employeeService.repositories.DepartmentRepository;
+import com.codeclan.example.employeeService.repositories.EmployeeRepository;
+import com.codeclan.example.employeeService.repositories.ProjectRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class EmployeeServiceApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Autowired
+	DepartmentRepository departmentRepository;
+
+	@Autowired
+	ProjectRepository projectRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createEmployeeAndDepartment() {
+		Department department = new Department("Technology");
+		departmentRepository.save(department);
+
+		Employee employee = new Employee("Sky", "Su", "E41I", department);
+		employeeRepository.save(employee);
 	}
 
 }

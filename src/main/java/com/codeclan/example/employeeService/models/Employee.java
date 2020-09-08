3c5f4856f73511a54a8209a9b92d72cc49ed4690
Helena <inger.helena@gmail.com>
2020-09-08 11:06:14 +0100

    created files/packages

---
+++ src/main/java/com/codeclan/example/employeeService/models/Employee.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.employeeService.models;
+
+public class Employee {
+}
---
+++ src/main/java/com/codeclan/example/employeeService/repositories/EmployeeRepository.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.employeeService.repositories;
+
+public interface EmployeeRepository {
+}
